e601c8ed7a0f3542e5840e0b5abdccf65b3ae6ed
Dmytro2V <op2vin@gmail.com>
2023-03-20 22:31:21 +0200

    Phase4 logout done

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -23,4 +23,19 @@ router.post('/', async (req, res, next) => {
     return res.json({user});    
 });
 
+// Log out
+router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+  );
+
+
+
+
+
+
+
 module.exports = router;
